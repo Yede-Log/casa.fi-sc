73a3499ac41a72fe466b675c034cb7a0fdfffb56
Sangat Das <sangatdas5@gmail.com>
2023-12-08 22:53:01 +0530

    Compile smart contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,39 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+        optimizer: {
+            enabled: true,
+            runs: 100
+        },
+        viaIR: true
+    }
+  },
+  networks: {
+    polygon_mumbai: {
+        url: 'https://rpc-mumbai.maticvigil.com',
+        accounts: [vars.get("ACCOUNT_PRIVATE_KEY")]
+    },
+    polygon_zkevm: {
+        url: "",
+        accounts: [vars.get("ACCOUNT_PRIVATE_KEY")]
+    },
+    eth_sepolia: {
+        url: 'http://localhost:8546',
+        accounts: [vars.get("ACCOUNT_PRIVATE_KEY")]
+    },
+    linea: {
+        url: "",
+        accounts: [vars.get("ACCOUNT_PRIVATE_KEY")]
+    },
+    base_goerli: {
+        url: '',
+        accounts: [vars.get("ACCOUNT_PRIVATE_KEY")]
+    }
+  }
 };
 
 export default config;
