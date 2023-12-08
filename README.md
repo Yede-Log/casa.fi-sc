1aba5a5b1d388621aca078c039ac98913930229a
Abhiavati20 <abhishekaa1112@gmail.com>
2023-12-09 03:08:03 +0530

    new real estate nft deployed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # casa.fi-sc
+new nft contract address 0xC97c006bdc45F176F40346baC0ccDb2C33284b80
--- contracts/RealEstateNft.sol
+++ contracts/RealEstateNft.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.19;
 // for implementing ERC721 standards
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-// Ownable will be used to restrict some functionalities and used only by contract owner
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract RealEstateNft is ERC721URIStorage, Ownable {
+contract RealEstateNft is ERC721URIStorage {
     // a counter for real estate tokens
     uint256 private realEstateCounter;
 
     // constructor
-    constructor() Ownable(msg.sender) ERC721("RealEstateNft", "REN") {
+    constructor() ERC721("RealEstateNft", "REN") {
         // so first real estate token minted it will have an id = 1
         realEstateCounter = 1;
     }
@@ -35,25 +32,23 @@ contract RealEstateNft is ERC721URIStorage, Ownable {
         string location,
         uint256 squareFeetArea,
         uint256 price,
-        bool forSale,
-        string realEstateTokenURI
+        bool forSale
     );
 
     // Event will be triggered if a real estate is being listed for sale
     event RealEstateForSale(uint256 realEstateNftID, uint256 price);
 
     // Event will be triggered if a real estate is being sold to someone
-    event RealEstateSold(uint256 realEstateNftID, address buyer, uint256 price);
+    // event RealEstateSold(uint256 realEstateNftID, address buyer, uint256 price);
 
     // Function to tokenize real estate and mint a NFT
     function mintRealEstate(
         string memory _location,
         uint256 _squareFeetArea,
-        uint256 _price,
-        string memory realEstateTokenURI
-    ) external onlyOwner {
-        require(_squareFeetArea > 0, "Area must be greater than 0 square foot");
-        require(_price > 0, "Price of real estate must be greater than 0");
+        uint256 _price
+    ) external {
+        // require(_squareFeetArea > 0, "Area must be greater than 0 square foot");
+        // require(_price > 0, "Price of real estate must be greater than 0");
 
         RENft memory newRealEstateNft = RENft({
             location: _location,
@@ -64,7 +59,7 @@ contract RealEstateNft is ERC721URIStorage, Ownable {
 
         // minting the nft and assign it to current owner only along with it's metadata
         _mint(msg.sender, realEstateCounter);
-        _setTokenURI(realEstateCounter, realEstateTokenURI);
+        // _setTokenURI(realEstateCounter, realEstateTokenURI);
 
         realEstates[realEstateCounter] = newRealEstateNft;
 
@@ -74,8 +69,7 @@ contract RealEstateNft is ERC721URIStorage, Ownable {
             _location,
             _squareFeetArea,
             _price,
-            false,
-            realEstateTokenURI
+            false
         );
 
         // incrementing the counter;
@@ -83,49 +77,49 @@ contract RealEstateNft is ERC721URIStorage, Ownable {
     }
 
     // Function to make a real estate available for sale or auction
-    function updateRealEstateForSale(
-        uint256 _realEstateId,
-        uint256 price
-    ) external onlyOwner {
-        // some pre-checks
-        require(
-            ownerOf(_realEstateId) == msg.sender,
-            "You are not the owner of real estate"
-        );
-        require(price > 0, "Price must be greater than 0");
+    // function updateRealEstateForSale(
+    //     uint256 _realEstateId,
+    //     uint256 price
+    // ) external onlyOwner {
+    //     // some pre-checks
+    //     require(
+    //         ownerOf(_realEstateId) == msg.sender,
+    //         "You are not the owner of real estate"
+    //     );
+    //     require(price > 0, "Price must be greater than 0");
 
-        // update the real estate object
-        realEstates[_realEstateId].forSale = true;
-        realEstates[_realEstateId].price = price;
+    //     // update the real estate object
+    //     realEstates[_realEstateId].forSale = true;
+    //     realEstates[_realEstateId].price = price;
 
-        // trigger the event
-        emit RealEstateForSale(_realEstateId, price);
-    }
+    //     // trigger the event
+    //     emit RealEstateForSale(_realEstateId, price);
+    // }
 
-    function buyRealEstate(uint256 _realEstateId) external payable {
-        // some pre-checks
-        require(
-            realEstates[_realEstateId].forSale,
-            "Real Estate is not FOR SALE!!"
-        );
-        require(
-            msg.value == realEstates[_realEstateId].price,
-            "Kindly enter the appropriate amount"
-        );
+    // function buyRealEstate(uint256 _realEstateId) external payable {
+    //     // some pre-checks
+    //     require(
+    //         realEstates[_realEstateId].forSale,
+    //         "Real Estate is not FOR SALE!!"
+    //     );
+    //     require(
+    //         msg.value == realEstates[_realEstateId].price,
+    //         "Kindly enter the appropriate amount"
+    //     );
 
-        address seller = ownerOf(_realEstateId);
+    //     address seller = ownerOf(_realEstateId);
 
-        // transfering the ownership of real estate to the buyer
-        _transfer(seller, msg.sender, _realEstateId);
+    //     // transfering the ownership of real estate to the buyer
+    //     _transfer(seller, msg.sender, _realEstateId);
 
-        // real estate is no longer required for sale
-        realEstates[_realEstateId].forSale = false;
+    //     // real estate is no longer required for sale
+    //     realEstates[_realEstateId].forSale = false;
 
-        emit RealEstateSold(_realEstateId, msg.sender, msg.value);
+    //     emit RealEstateSold(_realEstateId, msg.sender, msg.value);
 
-        // pay the price to seller
-        payable(seller).transfer(msg.value);
-    }
+    //     // pay the price to seller
+    //     payable(seller).transfer(msg.value);
+    // }
 
     // function to get all real estates
     function getAllRealEstates() external view returns (RENft[] memory) {
